a4efae8fc5834fa0e0cd54f38933fba7220fa352
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-04-26 21:32:22 +0100

    Further small changes

--- game.js
+++ game.js
@@ -22,8 +22,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       // Fetch the judoka and gokyo data
-      const judokaData = await fetch("./data/judoka.json").then((response) => response.json());
-      const gokyoData = await fetch("./data/gokyo.json").then((response) => response.json());
+      const judokaData = await fetch("/judokon/data/judoka.json").then((response) => response.json());
+      const gokyoData = await fetch("/judokon/data/gokyo.json").then((response) => response.json());
 
       console.log("Judoka data fetched:", judokaData);
       console.log("Gokyo data fetched:", gokyoData);
