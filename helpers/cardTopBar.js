6dd0b3e6772a744c7c10a321363c5880fecb7aef
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-04-26 23:52:30 +0100

    More testing

--- helpers/cardTopBar.js
+++ helpers/cardTopBar.js
@@ -1,6 +1,21 @@
 const PLACEHOLDER_FLAG_URL = "./assets/countryFlags/placeholder-flag.png";
 
 /**
+ * Escapes special HTML characters in a string to prevent XSS.
+ * @param {string} str - The string to escape.
+ * @returns {string} The escaped string.
+ */
+function escapeHTML(str) {
+  if (typeof str !== "string") return str;
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+/**
  * Generates the top bar HTML for a judoka card, including name and flag.
  * @param {Object|null|undefined} judoka - The judoka object.
  * @param {string} [flagUrl] - The URL of the flag image.
