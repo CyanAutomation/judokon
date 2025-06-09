57fc586f2e26b0c126a0aee8a2ae665983b59d28
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-06-09 23:27:16 +0100

    Update View Judoka aria label (#194)

--- playwright/browse-judoka.spec.js
+++ playwright/browse-judoka.spec.js
@@ -1,8 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-// locator for the country dropdown label in carouselJudoka.html
 const FILTER_BY_COUNTRY_LOCATOR = /^Filter by Country:$/i;
-
 
 test.describe("Browse Judoka screen", () => {
   test.beforeEach(async ({ page }) => {
