79d8079339db3ea48302cac4ff95c330bd53f9d4
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-06-10 13:49:00 +0100

    Add JSDoc for debug logging utilities (#222)

--- src/helpers/debug.js
+++ src/helpers/debug.js
@@ -1,7 +1,17 @@
+/**
+ * Indicates whether debug messages should be printed.
+ * Set `DEBUG_LOGGING=true` in the environment or on `window` to enable.
+ * @type {boolean}
+ */
 export const DEBUG_LOGGING =
   (typeof process !== "undefined" && process.env.DEBUG_LOGGING === "true") ||
   (typeof window !== "undefined" && window.DEBUG_LOGGING === true);
 
+/**
+ * Log provided arguments when debugging is enabled.
+ * @param {...unknown} args - Data passed to `console.log`.
+ * @returns {void}
+ */
 export function debugLog(...args) {
   if (DEBUG_LOGGING) {
     console.log(...args);
