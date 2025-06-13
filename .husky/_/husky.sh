5d43a4b829fafd632db029b85c0fcc714440f911
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-06-13 03:25:16 +0000

    PR: Auto-formatting and LINT fixes

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,10 +1,9 @@
-cat <<EOF
-husky - DEPRECATED
+echo "husky - DEPRECATED
 
 Please remove the following two lines from $0:
 
 #!/usr/bin/env sh
-. "$(dirname -- "$0")/_/husky.sh"
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
 They WILL FAIL in v10.0.0
-EOF
+"
