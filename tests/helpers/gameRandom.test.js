5f1b5aed2a390378c52420366edab6e3cd8ffd5e
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-07-26 23:35:01 +0100

    Update test for new random card options (#1119)

--- tests/helpers/gameRandom.test.js
+++ tests/helpers/gameRandom.test.js
@@ -32,6 +32,13 @@ describe("game.js", () => {
     document.dispatchEvent(new Event("DOMContentLoaded"));
     showRandom.dispatchEvent(new Event("click"));
 
-    expect(generateRandomCard).toHaveBeenCalledWith(null, null, gameArea, true);
+    expect(generateRandomCard).toHaveBeenCalledWith(
+      null,
+      null,
+      gameArea,
+      true,
+      undefined,
+      { enableInspector: false }
+    );
   });
 });
