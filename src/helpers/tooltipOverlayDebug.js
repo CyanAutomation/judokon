852dc3c8aff30223945083a950608981eb9145a2
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-08-03 22:29:34 +0000

    Update pseudocode in toggleTooltipOverlayDebug for clarity

--- src/helpers/tooltipOverlayDebug.js
+++ src/helpers/tooltipOverlayDebug.js
@@ -1,6 +1,10 @@
 /**
  * Toggle the tooltip debug overlay class on the document body.
  *
+ * @pseudocode
+ * 1. If document.body is not available, return immediately.
+ * 2. Toggle the "tooltip-overlay-debug" class on document.body based on `enabled`.
+ *
  * @param {boolean} enabled - Whether the overlay should be enabled.
  * @returns {void}
  */
