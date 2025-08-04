8e4cec1a3803cc0b349fcbc5071c1d23ec1ffeb9
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-08-04 15:11:43 +0100

    Dispatch scroll event on keyboard navigation

--- tests/helpers/keyboardNavigation.test.js
+++ tests/helpers/keyboardNavigation.test.js
@@ -6,6 +6,8 @@ describe("setupKeyboardNavigation", () => {
     const container = document.createElement("div");
     Object.defineProperty(container, "clientWidth", { value: 300, configurable: true });
     container.scrollLeft = 0;
+    const scrollEvents = [];
+    container.addEventListener("scroll", () => scrollEvents.push(true));
     document.body.append(container);
 
     setupKeyboardNavigation(container);
@@ -14,10 +16,12 @@ describe("setupKeyboardNavigation", () => {
     container.focus();
     container.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowRight" }));
     expect(container.scrollLeft).toBe(container.clientWidth);
+    expect(scrollEvents.length).toBe(1);
     expect(document.activeElement).toBe(container);
 
     container.dispatchEvent(new KeyboardEvent("keydown", { key: "ArrowLeft" }));
     expect(container.scrollLeft).toBe(0);
+    expect(scrollEvents.length).toBe(2);
     expect(document.activeElement).toBe(container);
   });
 
