d7273ca250fc826db40388d7149528bf82ce707d
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-10-15 23:10:34 +0100

    docs: expand battle helper jsdoc (#4095)

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,10 +1,11 @@
 /**
  * Determine if the current runtime should be treated as a development environment.
  *
+ * @returns {boolean} True when server or client flags indicate development mode.
  * @pseudocode
- * if process.NODE_ENV is "development" return true
- * else if window.__DEV__ is truthy return true
- * else return false
+ * 1. When `process.env.NODE_ENV` equals "development", return true.
+ * 2. Otherwise, if `window.__DEV__` is truthy, return true.
+ * 3. In all other cases, return false.
  */
 export function isDevelopmentEnvironment() {
   if (typeof process !== "undefined" && process.env?.NODE_ENV === "development") {
