e1883ba9a5ddc15bcbef2d9022596ab67b506967
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-11-04 21:34:41 +0000

    Use common Playwright fixtures in battle CLI restart spec

--- playwright/battle-cli-restart.spec.js
+++ playwright/battle-cli-restart.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "./fixtures/commonSetup.js";
 import { withMutedConsole } from "../tests/utils/console.js";
 import { configureApp } from "./fixtures/appConfig.js";
 
