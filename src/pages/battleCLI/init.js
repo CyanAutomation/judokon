5a50513c7d1d44974ec12744bc62df4c3126cf34
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-11-19 21:44:20 +0000

    Remove redundant toggle event dispatch in hideCliShortcuts function

--- src/pages/battleCLI/init.js
+++ src/pages/battleCLI/init.js
@@ -967,11 +967,6 @@ function hideCliShortcuts() {
   const sec = byId("cli-shortcuts");
   if (sec) {
     sec.open = false;
-    // Ensure toggle event is fired for listeners (test environments may not fire it automatically)
-    try {
-      const toggleEvent = new Event("toggle", { bubbles: false });
-      sec.dispatchEvent(toggleEvent);
-    } catch {}
   }
 }
 
