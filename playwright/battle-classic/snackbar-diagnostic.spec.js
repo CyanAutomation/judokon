c200da94ef820e07a640621af5a319fe4e7b13d1
CyanAutomation <68898852+CyanAutomation@users.noreply.github.com>
2025-12-28 10:58:22 +0000

    PR: Auto-formatting and LINT fixes (#5206)

--- playwright/battle-classic/snackbar-diagnostic.spec.js
+++ playwright/battle-classic/snackbar-diagnostic.spec.js
@@ -1,9 +1,6 @@
 import { test, expect } from "../fixtures/commonSetup.js";
 import { withMutedConsole } from "../../tests/utils/console.js";
-import {
-  waitForNextButtonReady,
-  waitForStatButtonsReady
-} from "../helpers/battleStateHelper.js";
+import { waitForNextButtonReady, waitForStatButtonsReady } from "../helpers/battleStateHelper.js";
 
 test.describe("Snackbar diagnostic tests", () => {
   test("selecting a stat shows snackbar and enables Next", async ({ page }) => {
